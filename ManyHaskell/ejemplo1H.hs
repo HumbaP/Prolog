bd02556317b9519f3f348dc51653d836ed5a760e
Many <manuel_md123@hotmail.com>
2017-10-09 10:55:12 -0600

    Ejemplos de inicio de haskell 09/10/2017

---
+++ ManyHaskell/ejemplo1H.hs
@@ -0,0 +1,14 @@
+size :: Int
+size = 12+13
+
+lista :: [Int]
+lista = [2,5,6,1,9]
+
+square :: Int -> Int
+square n = n * n
+
+suma :: Int -> Int -> Int 
+suma n m = n + m
+
+eligeSaludo :: Bool -> String
+eligeSaludo x = if x then "adios" else "hola"
