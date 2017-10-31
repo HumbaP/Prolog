eee91c7176c2dd1273efb49b8de7d278aafb5ccc
ManyAlex <manuel_md123@hotmail.com>
2017-10-31 10:54:57 -0700

    clase de manejo de listas 31/10/2017

---
+++ ManyHaskell/Listas2.hs
@@ -0,0 +1,13 @@
+
+
+
+--ziper :: [a] -> [b] -> [(a,b)]
+
+pairs :: [a] -> [(a,a)]
+pairs xs = zip xs (tail xs)
+
+sorted :: Ord a => [a] -> Bool
+sorted xs = and [x <= y | (x,y) <- pairs xs]
+
+positions :: Eq a => a -> [a] -> [Int]
+positions x xs = [i | (x',i) <- zip xs [0..], x == x']
