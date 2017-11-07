d1553be19a1f5f618c1d7bf520b71a5b41497724
Manuel Alejandro Medrano Díaz <manuel_md123@hotmail.com>
2017-11-07 10:57:16 -0700

    Ejercicios añadidos

--- ManyHaskell/Ejercicios.hs
+++ ManyHaskell/Ejercicios.hs
@@ -13,5 +13,41 @@ perfects :: Int -> [Int]
 --perfects n = [x | x <- [1..n], sum(init(factores x))==x]
 perfects n = [x | x <- [1..n], sum(factores x)-x==x]
 
+--suma los elementos multiplicados de 2 listas 
 scalar :: [Int] -> [Int] -> Int
-scalar l li = [x | x <- l+li]
+scalar l ls = product(l) + product(ls)
+
+--saca el factorial
+fac :: Int -> Int
+fac n = product[1..n]
+fac 0 = 1
+fac n = n * fac(n-1)
+
+--Multiplica los elementos de una lista
+producto :: Num a => [a] -> a
+producto [] = 1
+producto (n:ns) = n * producto ns
+
+--tamaño de una lista
+length1 :: [a] -> Int
+length1 [] = 0
+length1 (_:xs) = 1 + length1 xs
+
+--invertir una lista
+reverse1 :: [a] -> [a]
+reverse1 [] = []
+reverse1 (x:xs) = reverse xs ++ [x]
+
+zipi :: [a] -> [b] -> [(a,b)]
+zipi [] _ = []
+zipi _ [] = []
+zipi (x:xs)(y:ys) = (x,y) : zip xs ys
+
+drope :: Int -> [a] -> [a]
+drope 0 xs = xs
+drope _ [] = []
+drope n (_:xs) = drope (n-1) xs
+
+(++) :: [a] -> [a] -> [a]
+[] ++ ys = ys
+(x:xs) ++ ys = x:(xs ++ ys)
