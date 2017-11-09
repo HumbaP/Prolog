bade8098673062f6920c7a2ece03df357c9caa46
Manuel Alejandro Medrano Díaz <manuel_md123@hotmail.com>
2017-11-09 10:58:20 -0700

    Ejercicios de guardas y patrones y generadores parte 2

--- ManyHaskell/EjerciciosExamen.hs
+++ ManyHaskell/EjerciciosExamen.hs
@@ -5,6 +5,13 @@ menores n ls = [x | x<-ls, x < n]
 multiplos :: Int -> Int -> [Int]
 multiplos n m = [x | x <-[1..m],x `mod` n == 0]
 
+--dar un numero, y de respuesta una tupla con la lista de los numero pares, y otra lista de numeros impares
+paresImpares :: Int -> ([Int],[Int])
+paresImpares n = ([x | x <- [1,3..n]],[y | y<-[2,4..n]])
+
+
+
+
 --Patrones y guardas
 --1.- recibir la edad de una persona y decirle en que nivel escolar se esprea que este.
 nivel :: Int -> String
@@ -28,3 +35,29 @@ imc a p
 	| p/a^2<= 34.99 = "Obeso: Tipo I"
 	| p/a^2<= 40 = "Obeso: Tipo II"
 	| otherwise = "Obeso: Tipo III"
+
+--Potencia de un numero.
+potencia :: Int -> Int ->Int
+potencia base exp
+	| exp < 0 = 0
+	| exp == 0 = 1
+	| let = base^exp
+
+--con patron
+potencia2 :: Int -> Int -> Int
+potencia2 0 0 = 0
+potencia2 base 0 = 1
+potencia2 base expo = base ^expo
+
+--3 estado civil.
+estadoC :: String -> String
+estadoC cad 
+	| cad == "Soltero" = "Hola Guapo(a)"
+	| cad == "Casado" = "Estas fuera de mi alcance"
+	| cad == "Concuvinado" = "A medias"
+	| otherwise = "Indeterminado"
+estadoCivil :: String -> String
+estadoCivil "Soltero" = "Hola Guapo(a)"
+estadoCivil "Casado" = "Estas fuera de mi alcance"
+estadoCivil "Concuvinado" = "A medias"
+estadoCivil x = "Indeterminado"
